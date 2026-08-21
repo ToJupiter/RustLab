b8755e8041f535be0969fc3ae833163272a2a801
Hải Phan <ToJupiter>
2026-08-21 23:14:37 +0700

    feat: Update main.rs with comments

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+    // rand::random_range -> tu convert sang kieu
     let secret_number: i32 = rand::random_range(0..100);
     println!("The secret number is {secret_number}");
 
@@ -13,8 +14,16 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("Failed to read line");
     println!("You guessed: {guess}");
 
-    let mut final_number = secret_number + guess.trim().parse::<i32>().unwrap();
-    let mut final_number_2 = function_a::a_plus_b(secret_number, guess.trim().parse::<i32>().unwrap());
-    assert_ne!(final_number, final_number_2);
+    // guess.trim().parse::<dtype>().unwrap();
+    let final_number = secret_number + guess.trim().parse::<i32>().unwrap();
+    let final_number_2 = function_a::a_plus_b(secret_number, guess.trim().parse::<i32>().unwrap());
+    assert_eq!(final_number, final_number_2);
+
+    // match a.cmp(&b) {} -> must borrow b. 
+    match final_number.cmp(&secret_number) {
+        Ordering::Greater => println!(">"),
+        Ordering::Equal => println!("="),
+        Ordering::Less => println!("<")
+    }
 }
 
